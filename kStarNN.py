847c1793faf7c4263aace9ec599fe4f2a727b840
shuchu han <shuchu@nec-labs.com>
2017-08-02 16:54:25 -0400

    add the python implementation of kStarNN

---
+++ kStarNN.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+#coding="utf-8"
+
+"""
+K*-nearest-neighbor graph 
+
+reference:
+	2016 NIPS, Oren Anava, Kfir Levy, "k*-nearest Neighbors: From Global to Local"
+"""
+
+import sys
+import numpy
+import scipy
+
+class kStarNN:
+
+    def __init__(self):
+        pass
+
+    def generateGraph(self, data_file, libschitz_noise_ratio):
+        """ load data from data file and generate kStarNN graph.
+        the generated graph is stored in adjacent list format
+
+        parameters:
+            data_file -- input data matrix, each row is a sample.
+            libschitz_noise_ratio -- hyperparameter, control the bound of "k*"
+        """
+       
+        # step 1, for each sample, we calculate the distance between itself and
+        # samples. and then sort.
+
+    def one_to_others_distances(self, idx, data):
+        """ calculate the distance between sample "idx" and other rows in matrix data.
+        When calculate the distance, we set the distance between "idx" and itself to the 
+        MAXIMUM INT value.
+
+        parameters:
+            idx -- row index of current sample
+            data -- data matrix, each row is a sample
+        return:
+            dist -- one row of nparray of distances.
+        """
+
+    
+
+
